19eb2afca7423938ed09570f422cd821f64ac323
Arun8487 <36092721+Arun8487@users.noreply.github.com>
2018-02-03 08:33:57 +0530

    1st

---
+++ SBTest/src/main/java/com/testapp/ServicesApp.java
@@ -0,0 +1,22 @@
+package com.testapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ServicesApp {
+	
+	
+	@RequestMapping("/")
+   public String home() {
+        return "Hello World!";
+    }
+	
+	@RequestMapping("/getMsg")
+	public String getMsg()
+	{
+		return "HI Hello";
+	}
+
+}
